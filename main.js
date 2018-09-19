b1d64a6ee6cfba0640cc710c4b4ac86daa6a94e6
sashajustice <sabrinnasreddin@gmail.com>
2018-09-18 18:14:27 -0600

    Changes to look in queries

---
+++ main.js
@@ -0,0 +1,378 @@
+/*///////////BOTTOM HALF TABLE//////////////////*/
+.bottom-half {
+  background-color: #EEF2F6;
+  display:grid;
+  grid-template-columns: 1fr 25rem;
+  padding-top: 157px;
+  border-top: 1px solid #A1BFE2;
+}
+
+.table-header {
+background-color: #727E93
+background-color: #717e93;
+border-radius: 5px 5px 0 0;
+height: 40rem;
+height: 80px;
+padding:40px;
+width:70rem
+}
+
+
+.table-elements {
+  padding-top: 60px;
+}
+
+.th-job-description-left-part {
+   border-radius: 5px 0 0 0;
+  color: white;
+  font-size: 0.6rem;
+  padding: 20px 10px;
+  text-transform: uppercase;
+}
+
+.job-description-right-side {
+
+  border-radius: 0 5px 0 0;
+  padding-left: 20px;
+  text-align: left;
+
+}
+
+
+.table-edit {
+
+  background-color:#7B9CC6;
+}
+
+
+th {
+  background-color: #6f7d95;
+}
+
+td {
+  border-bottom: 1px solid #d8dde7;
+  color: #677d9c;
+  padding: 20px;
+}
+
+
+h2 {
+
+  margin-right: 50px;
+  font-color:#16a4fa;;
+
+}
+
+.job-details {
+
+  background-color: white;
+  border:solid 2px #E8EEF2;
+  margin-left: 20px;
+  padding-top: -40px;
+}
+
+/*SOCIAL MEDIA SECTION///////////*/
+
+.social {
+  border-radius: 5px;
+  color: white;
+  font-size: 1rem;
+  margin-bottom: 15px;
+  padding: 15px;
+  text-align: left;
+  width: 100%;
+}
+
+.social:hover {
+  cursor: pointer;
+  filter: brightness(120%);
+  transition: 300ms ease-in-out;
+}
+
+.social i{
+  padding-right: 8rem;
+}
+
+
+.percentage {
+  fill: #666;
+  font-family: sans-serif;
+  font-size: 0.25rem;
+  text-anchor: middle;
+}
+
+.share {
+  display: flex;
+  align-items: center; 
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 3rem;
+  margin-left: 30px;
+  margin-top: -8%;
+}
+
+
+.share-btn {
+  display: flex;
+  color; blue;
+  width: 91%;
+  height: 10%;
+  align-items: center; 
+  justify-content: space-between;
+  width: 100%;
+  margin: 40px;
+}
+
+.share-btn i {
+  padding-left: 15px;
+      width: 99%;
+    height: 10%;
+}
+
+.btn-placeholder {
+  padding-right: 15px;
+
+}
+
+
+.my-links-h2 {
+
+    color: #666;
+
+}
+
+
+.share-btn.share-facebook {
+  background-color: #5271BD;
+  color:white;
+  font-size: 1rem;
+}
+
+
+
+.share-btn.share-twitter {
+  background-color: #48A8E9;
+  color:white;
+  font-size: 1rem;
+}
+
+.share-btn.share-linkedin {
+background-color: #357EB7;
+color:white;
+font-size: 1rem;
+}
+
+ .social-media-buttons {
+   /* display: flex;
+    flex-direction:row;*/
+    width: 90%;
+    height: 75%;
+
+   }
+
+
+.fb-icon {
+  padding-left: 26px;
+}
+
+.twittr-icon {
+   padding-left: 26px;
+
+}
+
+.linkd-icon {
+  padding-left: 26px;
+
+}
+
+
+.my-links {
+  width: 100%;
+
+
+}
+.right-side-navbar {
+   padding: 0 50px 20px 50px;
+}
+
+.fab.fa-linkedin {
+  background-color: #0384bf;
+  border: none;
+  color: white;
+}
+
+.twitter-link {
+  background-color: #00aaef;
+  border: none;
+  color: white;
+}
+
+.fab.fa-facebook-f {
+  background-size: 18px;
+  background-color: #5071bc;
+  border: none;
+}
+
+.categories div {
+  padding: 5px 0;
+}
+
+/*===========BOTTOM RIGHT SIDE NAV ===================*/
+
+h5 {
+  color: #727a81;
+  font-size: 1rem;
+}
+
+h5 i{
+  padding-right: 5px;
+}
+
+.info-text {
+  color: #727a81;
+  font-size: 0.9rem;
+}
+
+
+
+.copy-btn {
+  background-color: #77ced6;
+  border-radius: 0 3px 3px 0;
+  border: 0;
+  color: white;
+  font-size: 1rem;
+  margin-left: 0;
+  padding: 15px 0;
+  width: 15%;
+}
+
+.copy-btn:hover {
+  cursor: pointer;
+
+}
+
+.copy-btn::placeholder {
+  color: #a9afb8;
+}
+
+
+.linkedin-link {
+  background-color: #0384bf;
+  border: none;
+  color: white;
+}
+
+.link-input {
+  border: none;
+  font-size: 1rem;
+  height: 54px;
+  margin: 0;
+  padding-left: 1rem;
+  width: 80%
+}
+
+.link-input.email {
+  width: 100%
+}
+
+.right-side-nav-container {
+  padding: 0 50px 20px 50px;
+}
+
+
+
+.copy-button {
+  background-color: #77ced6;
+  border-radius: 0 3px 3px 0;
+  border: 0;
+  color: white;
+  font-size: 1rem;
+  margin-left: 0;
+  padding: 15px 0;
+  width: 15%;
+}
+
+/*.copy-button:hover {
+  cursor: pointer;
+  filter: brightness(110%);
+  transition: 300ms ease-in-out;
+}
+
+.copy-button::placeholder {
+  color: #a9afb8;
+}*/
+
+.facebook-link {
+  background-color: #4c72c3;
+  border: none;
+}
+
+/*h5 {
+  color: #727a81;
+  font-size: 1rem;
+}
+
+h5 i{
+  padding-right: 5px;
+}*/
+
+.info-text {
+  color: #727a81;
+  font-size: 0.9rem;
+}
+
+.linkedin-link {
+  background-color: #0384bf;
+  border: none;
+  color: white;
+}
+
+.link-input {
+  border: none;
+  font-size: 1rem;
+  height: 54px;
+  margin: 0;
+  padding-left: 1rem;
+  width: 80%
+}
+
+.link-input.email {
+  width: 100%
+}
+
+.right-side-nav-container {
+  padding: 0 50px 20px 50px;
+}
+
+.social {
+  border-radius: 5px;
+  color: white;
+  font-size: 1rem;
+  margin-bottom: 15px;
+  padding: 15px;
+  text-align: left;
+  width: 100%;
+}
+
+.social:hover {
+  cursor: pointer;
+  filter: brightness(120%);
+  transition: 300ms ease-in-out;
+}
+
+.social i{
+  padding-right: 8rem;
+}
+
+
+
+.twitter-link {
+  background-color: #00aaef;
+  border: none;
+  color: white;
+}
+
+
+.sidebar {
+    display:none;
+  }
